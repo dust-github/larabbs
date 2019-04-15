a78e1b62b5e4e0318d5fde1b085f793f432bbe70
Dust <1036605035@qq.com>
2019-04-15 07:19:05 +0000

    WYSIWYG 编辑器

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-
 @section('content')
 
   <div class="container">
@@ -54,5 +53,22 @@
       </div>
     </div>
   </div>
-
 @endsection
+
+@section('styles')
+  <link rel="stylesheet" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+  <script>
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
+@stop
